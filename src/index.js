77dbbd38e71c85eb9dceb371bab6120f2dfc65b2
Kelly Rogers <kellyerogers92@gmail.com>
2024-01-11 16:46:37 -0700

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (event) =>{
+    event.preventDefault()
+    // console.log(event.target.newTask.value)
+    addNewTask(event.target.newTask.value, event.target.priority.value)
+    let priority = event.target.priority.value
+  })
+  });
+
+  function addNewTask(todo, priority){
+    let li = document.createElement ('li');
+    li.textContent = `Task: ${todo}, Priority: ${priority}` 
+    li.style.color = priority.toLowerCase() === 'high' ? 'red' : 'green'; 
+    
+    let btn = document.createElement('button')
+    btn.innerText='x';
+    btn.addEventListener('click', ()=> {
+      removeTask(li)
+    })
+
+    li.appendChild(btn);
+    
+    document.getElementById('tasks').appendChild(li)
+
+  }
+
+
+  function removeTask(task){
+    task.remove()
+  }
+
+  
